1480628575214eaec7c036eceabdfe4b01d32473
ThomasEtges <thomas.etges.10@gmail.com>
2025-10-08 11:56:10 -0300

    começando o adicionar task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,12 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.auth)
+    implementation("com.google.firebase:firebase-auth")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/desafio1_mobile/HomePageActivity.java
+++ app/src/main/java/com/example/desafio1_mobile/HomePageActivity.java
@@ -4,6 +4,7 @@ import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -41,9 +43,14 @@ public class HomePageActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         Button btnGoToLogin = findViewById(R.id.btnGoToLogin);
+        FloatingActionButton btnAddTask = findViewById(R.id.btnAddTask);
 
         btnGoToLogin.setOnClickListener(v -> showLoginBottomSheet());
+        btnAddTask.setOnClickListener(v -> showAddTask());
+
     }
+
+    //////////////----TUDO DE LOGIN E CADASTRO----//////////////////
 
     private void showLoginBottomSheet() {
 
@@ -55,29 +62,51 @@ public class HomePageActivity extends AppCompatActivity {
         Button btnClose = bottomSheetDialog.findViewById(R.id.btnClose);
         Button btnGoToRegister = bottomSheetDialog.findViewById(R.id.btnGoToRegister);
 
-        btnLogin.setOnClickListener( v-> {
+        assert btnLogin != null;
+        btnLogin.setOnClickListener(v-> {
             String email = inputEmail.getText().toString();
             String password = inputPassword.getText().toString();
-            validateInputLogin(email,password);
+            validateInputLogin(email,password,bottomSheetDialog);
         });
 
+        assert btnClose != null;
         btnClose.setOnClickListener(v -> {
-            bottomSheetDialog.dismiss();
+            closBottomSheet(bottomSheetDialog);
         });
 
+        assert btnGoToRegister != null;
         btnGoToRegister.setOnClickListener(v -> {
-                bottomSheetDialog.dismiss();
-                showRegisterBottomSheet();
+            closBottomSheet(bottomSheetDialog);
+            showRegisterBottomSheet();
         });
 
         bottomSheetDialog.show();
     }
 
-    private void validateInputLogin(String email, String password){
+    private void closBottomSheet(BottomSheetDialog bottomSheetDialog){
+        bottomSheetDialog.dismiss();
+    }
+
+    private void validateInputLogin(String email, String password, BottomSheetDialog bottomSheetDialog){
         if(email.isEmpty() || password.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Preencha todos os campos", Toast.LENGTH_SHORT).show();
         }else{
-            login(email,password);
+            login(email,password,bottomSheetDialog);
+        }
+    }
+
+    private void validateInputRegister(String email, String password, String name, BottomSheetDialog bottomSheetDialog) {
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(getApplicationContext(), "Email inválido", Toast.LENGTH_SHORT).show();
+        }
+        else if(password.length() < 6){
+            Toast.makeText(getApplicationContext(), "Senha deve ter no mínimo 6 caracteres", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            registerUser(email,password, bottomSheetDialog);
         }
     }
 
@@ -85,16 +114,30 @@ public class HomePageActivity extends AppCompatActivity {
 
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(R.layout.bottom_sheet_register);
+        EditText inputEmailRegister = bottomSheetDialog.findViewById(R.id.inputEmailRegister);
+        EditText inputPasswordRegister = bottomSheetDialog.findViewById(R.id.inputPasswordRegister);
+        EditText inputNameRegister = bottomSheetDialog.findViewById(R.id.inputNameRegister);
 
+        Button btnRegister = bottomSheetDialog.findViewById(R.id.btnRegister);
         Button btnClose = bottomSheetDialog.findViewById(R.id.btnClose);
         Button btnGoToLogin = bottomSheetDialog.findViewById(R.id.btnGoToLogin);
 
-        btnClose.setOnClickListener(v -> {
-            bottomSheetDialog.dismiss();
+        assert btnRegister != null;
+        btnRegister.setOnClickListener( v-> {
+            String email = inputEmailRegister.getText().toString();
+            String password = inputPasswordRegister.getText().toString();
+            String name = inputNameRegister.getText().toString();
+            validateInputRegister(email,password,name,bottomSheetDialog);
         });
 
+        assert btnClose != null;
+        btnClose.setOnClickListener(v -> {
+            closBottomSheet(bottomSheetDialog);
+        });
+
+        assert btnGoToLogin != null;
         btnGoToLogin.setOnClickListener(v -> {
-                bottomSheetDialog.dismiss();
+            closBottomSheet(bottomSheetDialog);
                 showLoginBottomSheet();
         });
 
@@ -102,7 +145,7 @@ public class HomePageActivity extends AppCompatActivity {
     }
 
 
-    public void login(String email, String password){
+    public void login(String email, String password, BottomSheetDialog bottomSheetDialog){
         mAuth.signInWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -111,6 +154,7 @@ public class HomePageActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCustomToken:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            closBottomSheet(bottomSheetDialog);
                             //updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -123,7 +167,28 @@ public class HomePageActivity extends AppCompatActivity {
                 });
     }
 
-
+    public void registerUser(String email, String password, BottomSheetDialog bottomSheetDialog) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(),"Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+                            closBottomSheet(bottomSheetDialog);
+                            showLoginBottomSheet();
+                            //updateUI(user);
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(),
+                                    task.getException().getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
 
     public void logout(){
         FirebaseAuth.getInstance().signOut();
@@ -135,30 +200,13 @@ public class HomePageActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
     }
+
+    //////////////----CRIAÇÃO DE TAREFAS----//////////////////
+
+    private void showAddTask(){
+
+    }
+
 }
 
 
-//public void cadastrarUsuario(String email, String password) {
-//    mAuth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                @Override
-//                public void onComplete(@NonNull Task<AuthResult> task) {
-//                    if (task.isSuccessful()) {
-//                        // Cadastro bem-sucedido
-//                        Log.d(TAG, "createUserWithEmail:success");
-//                        FirebaseUser user = mAuth.getCurrentUser();
-//                        Toast.makeText(getApplicationContext(),
-//                                "Usuário cadastrado com sucesso!",
-//                                Toast.LENGTH_SHORT).show();
-//                        //updateUI(user);
-//                    } else {
-//                        // Falha no cadastro
-//                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
-//                        Toast.makeText(getApplicationContext(),
-//                                "Erro ao cadastrar: " + task.getException().getMessage(),
-//                                Toast.LENGTH_LONG).show();
-//                        //updateUI(null);
-//                    }
-//                }
-//            });
-//}
